2f0b2345d7e675f7f7a4d1cbc8f51de2531d24f5
David Andersson <david.andersson@zocom.se>
2023-04-19 14:46:05 +0200

    Preparation for AJAX

--- src/components/viewSnippets/ViewSnippets.jsx
+++ src/components/viewSnippets/ViewSnippets.jsx
@@ -1,59 +1,94 @@
+import { useState, useEffect } from 'react'
 import './ViewSnippets.css'
 
 // 1. Skapa (tillfällig) data, utifrån det som API:et returnerar - en array med minst 1 objekt
 // 2. Använd map-funktionen för att rendera <div className="vote"> för varje objekt
 const tempData = [
 	{
-		??????
+		"id": 1348,
+		"content": "while(true){\n echo \"42\\n\";\n}",
+		"score": 3,
+	},
+	{
+		"id": 134,
+		"content": "// test",
+		"score": 12,
 	}
 ]
 
-const ViewSnippets = () => (
-	<div className="component">
+const ViewSnippets = () => {
+	const [snippets, setSnippets] = useState(null)
 
-		<section className="framed">
-			<span className="intro">
-				The latest snippets from coders like you!
-			</span>
-		</section>
+	useEffect(() => {
+		async function getLatestSnippets() {
+			// 1. skicka request till API:et (fråga efter latest snippets)
+			// 2. ta emot resultatet i JSON-format
+			// 3. spara i state-variabel
+		}
+	}, [])
 
-		<section>
-			{/*<div className="vote">
-				<code>let x=5;</code>
-				<div className="vote-buttons">
-					<button className="vote">🗑️</button>
-					<button className="vote">✍️</button>
-					<button className="vote">👍</button>
-					<button className="vote">👎</button>
-					<span className="score">5</span>
-				</div>
-			</div>
+	return (
+		<div className="component">
 
-			<div className="vote">
-				<textarea className="code" rows="4" value="let y = 8;" />
-				<div className="vote-buttons">
-					<button className="vote">✔️</button>
-					<button className="vote">❌</button>
-				</div>
-			</div>
+			<section className="framed">
+				<span className="intro">
+					The latest snippets from coders like you!
+				</span>
+			</section>
 
-
-			<div className="vote">
-				<code>
-				// Get a button
-					let btn = document.querySelector('&amp;my-button')
-				</code>
-				<div className="vote-buttons">
-					<button className="vote">🗑️</button>
-					<button className="vote">✍️</button>
-					<button className="vote">👍</button>
-					<button className="vote">👎</button>
-					<span className="score">25</span>
-				</div>
-</div>*/}
-		</section>
-		<hr />
-	</div>
-)
+			<section>
+				{snippets === null
+					? <p> Please wait, retrieving data... </p>
+					: snippets.map(snippet => (
+					<div key={snippet.id} className="vote">
+						<code> {snippet.content} </code>
+						<div className="vote-buttons">
+							<button className="vote">🗑️</button>
+							<button className="vote">✍️</button>
+							<button className="vote">👍</button>
+							<button className="vote">👎</button>
+							<span className="score"> {snippet.score} </span>
+						</div>
+					</div>
+				))}
+				</section>
+			<hr />
+		</div>
+	)
+}
 
 export default ViewSnippets
+
+/*<div className="vote">
+<code>let x=5;</code>
+<div className="vote-buttons">
+	<button className="vote">🗑️</button>
+	<button className="vote">✍️</button>
+	<button className="vote">👍</button>
+	<button className="vote">👎</button>
+	<span className="score">5</span>
+</div>
+</div>
+
+<div className="vote">
+<textarea className="code" rows="4" value="let y = 8;" />
+<div className="vote-buttons">
+	<button className="vote">✔️</button>
+	<button className="vote">❌</button>
+</div>
+</div>
+
+
+<div className="vote">
+<code>
+// Get a button
+	let btn = document.querySelector('&amp;my-button')
+</code>
+<div className="vote-buttons">
+	<button className="vote">🗑️</button>
+	<button className="vote">✍️</button>
+	<button className="vote">👍</button>
+	<button className="vote">👎</button>
+	<span className="score">25</span>
+</div>
+</div>*/
