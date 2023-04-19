1d4515498c3622c6ec0dde25b1944adc7c239a8e
David Andersson <david.andersson@zocom.se>
2023-04-19 11:57:04 +0200

    JSX split into feature components

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,14 @@
+import './Menu.css'
+
+const Menu = () => (
+	<>
+	<section className="menu component">
+		<button className="selected"> Latest </button>
+		<button> Best </button>
+		<button> Upload new </button>
+	</section>
+	<hr />
+	</>
+)
+
+export default Menu
